2e1b98f1fcae3a686fda31804882c7a09f040a7a
pacificstate01 <javier.otarola@inacapmail.cl>
2025-09-22 21:26:42 -0300

    El exlusivo i.m.a no era parte, asi que se empezo por el paso 1 del formulario, por terminar.

--- Frontend/FrontEndMuni/src/App.jsx
+++ Frontend/FrontEndMuni/src/App.jsx
@@ -1,28 +1,31 @@
 import { useState } from 'react'
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate,Outlet } from 'react-router-dom';
 import Login from './routes/login.jsx';
-import Formulario2A from './routes/Formulario2A/Formulario2A.jsx'
 import Formulario2A1 from './routes/Formulario2A/Formulario2A1.jsx';
 import Formulario2A2 from './routes/Formulario2A/Formulario2A2.jsx';
 import Formulario2A3 from './routes/Formulario2A/Formulario2A3.jsx';
 import Formulario2A4 from './routes/Formulario2A/Formulario2A4.jsx';
 import Formulario2A5 from './routes/Formulario2A/Formulario2A5.jsx';
 import Formulario2A6 from './routes/Formulario2A/Formulario2A6.jsx';
+const Form2ALayout = () => {
+  return <Outlet />;
+};
 function App() {
   return (
     <BrowserRouter>
-        <Routes>
-          <Route path ='/' element={<Login />}/>
-          <Route path ='/form2a' element={<Formulario2A />}>
-                  <Route path="paso1" element={<Formulario2A1/>} />
-                  <Route path="paso2" element={<Formulario2A2 />} />
-                  <Route path="paso3" element={<Formulario2A3 />} />
-                  <Route path="paso4" element={<Formulario2A4 />} />
-                  <Route path="paso5" element={<Formulario2A5 />} />
-                  <Route path="paso6" element={<Formulario2A6 />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/form2a' element={<Form2ALayout />}>
+          <Route index element={<Navigate to="paso1" replace />} />
+          <Route path="paso1" element={<Formulario2A1/>} />
+          <Route path="paso2" element={<Formulario2A2 />} />
+          <Route path="paso3" element={<Formulario2A3 />} />
+          <Route path="paso4" element={<Formulario2A4 />} />
+          <Route path="paso5" element={<Formulario2A5 />} />
+          <Route path="paso6" element={<Formulario2A6 />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
